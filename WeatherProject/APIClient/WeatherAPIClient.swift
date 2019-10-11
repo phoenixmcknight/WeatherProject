c5acd5640baf31bb2444d064808fd9b819b76bd6
phoenixmcknight <phoenixmcknight@gmail.com>
2019-10-10 21:45:38 -0400

    creates apiclient for weatherAPIModel

--- WeatherProject/APIClient/WeatherAPIClient.swift
+++ WeatherProject/APIClient/WeatherAPIClient.swift
@@ -13,7 +13,7 @@ class WeatherApiClient {
     
     static let shared = WeatherApiClient()
     
-    func getWeather(latLong:String, completionHandler:@escaping(Result<Daily,AppError>) -> Void) {
+    func getWeather(latLong:String, completionHandler:@escaping(Result<[DailyDatum],AppError>) -> Void) {
         let url = "https://api.darksky.net/forecast/0a592c9ebcf436a0d169213c2e2f899c/\(latLong)"
        
         guard let urlString = URL(string:url) else {
@@ -28,7 +28,7 @@ class WeatherApiClient {
                     completionHandler(.failure(error))
                 case.success(let data):
                     do { let weatherData = try JSONDecoder().decode(WeatherModel.self, from: data)
-                        completionHandler(.success(weatherData.daily))
+                        completionHandler(.success(weatherData.daily.data))
                     } catch{
         completionHandler(.failure(.invalidJSONResponse))
                     }
